234d7a2faffdfb5d92d15fa84f213f678f3df39c
Isaac Sousa <isaac.sousa.1202@gmail.com>
2024-10-03 13:36:16 -0300

    feat: implementing login and register pages

--- finmilyApp/src/app/tabs/tabs-routing.module.ts
+++ finmilyApp/src/app/tabs/tabs-routing.module.ts
@@ -25,14 +25,14 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: '/tabs/home',
+        redirectTo: '/login',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/home',
+    redirectTo: '/login',
     pathMatch: 'full'
   }
 ];
